489b51ad0170ec9d6a9f76a17ba1a4f8cfe1559e
ping <shiping.lim@outlook.com>
2021-06-22 01:33:02 +0800

    travis yml change

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,4 @@
+echo "Building deployment container"
+docker build -tag s3test-website-deploy .
 echo "Deploying to S3"
-docker run $(printenv | grep -E '^AWS' | sed 's/AWS_/-e /g' | sed 's/-e /-e AWS_/g') s3test-website
+docker run $(printenv | grep -E '^AWS' | sed 's/AWS_/-e /g' | sed 's/-e /-e AWS_/g') s3test-website-deploy
